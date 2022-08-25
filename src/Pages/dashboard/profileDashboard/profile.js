db1d9b99397c3d94813f72c09d6ab9830c056eb6
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-08-25 20:50:55 +0530

    profile page done

--- src/Pages/dashboard/profileDashboard/profile.js
+++ src/Pages/dashboard/profileDashboard/profile.js
@@ -1,15 +1,28 @@
 import React from 'react'
-import HomeLayout from '../../../layout/Home.layout'
+import HomeLayout from '../../../layout/Home.layout';
+import { BiLogOut } from 'react-icons/bi';
+import { IoSettingsOutline } from 'react-icons/io5';
 
 const Profile = () => {
-  return (
-    <HomeLayout>
-        <div>
-            <p className='text-white'>Hey, Shubhransh</p>
-            <hr className='class="border border-gray-700 cursor-pointer hover:border-red-500 duration-500"'/>
-        </div>
-    </HomeLayout>
-  )
+    return (
+        <HomeLayout>
+            <div>
+                <div className='w-full flex justify-between items-center '>
+                    <p className='text-white text-xl mb-2 mt-10'>Hey, Shubhransh</p>
+                    <div className='text-gray-500 flex items-center gap-6'><IoSettingsOutline size={'1.5rem'}/><BiLogOut size={'1.5rem'} className='' /></div>
+                </div>
+                <hr className='class="border border-gray-700 cursor-pointer hover:border-red-500 duration-500"' />
+                <br />
+                <br />
+                <div className='flex flex-wrap justify-between'>
+                    <div className='text-center border border-gray-400 w-80 lg:w-96 h-56 text-gray-300 mb-4'>Metaverse</div>
+                    <div className='text-center border border-gray-400 w-80 lg:w-96 h-56 text-gray-300 mb-4'>artistverse</div>
+                    <div className='text-center border border-gray-400 w-80 lg:w-96 h-56 text-gray-300 mb-4'>nft marketplace</div>
+                </div><br />
+                <h2 className='text-white font-semibold text-xl'>Your Daily Statistics</h2>
+            </div>
+        </HomeLayout>
+    )
 }
 
 export default Profile
